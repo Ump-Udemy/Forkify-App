4c96b1508d5cca19e6923f9593fa987dba526fbc
Umpz <46953365+PeerawatUmp@users.noreply.github.com>
2021-02-03 21:24:41 +0700

    storing bookmarks with localStorage

--- src/js/controller.js
+++ src/js/controller.js
@@ -22,15 +22,18 @@ const controlRecipes = async function () {
     recipeView.renderSpinner();
     //0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultPage());
+
+    //1) Updating bookmarks view
     bookmarksView.update(model.state.bookmarks);
 
-    //1 Loading recipe
+    //2) Loading recipe
     await model.loadRecipe(id);
 
-    //2 Rendering Recipe
+    //3) Rendering Recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
+    console.error(err);
   }
 };
 
@@ -84,7 +87,11 @@ const controlAddBookmark = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerBookmark(controlAddBookmark);
